40242ef04ecc44e0ea6219e8470a00a1f3d52a11
mpa12 <maksim.28.05@mail.ru>
2023-02-08 23:26:42 +1000

    Добавил поле is_admin в таблицу с users

---
+++ black-on-white/database/migrations/2023_02_08_130844_add_columns_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->nullable(false)->default(false);
+        });
+
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@mail.ru',
+            'password' => Hash::make('admin'),
+            'is_admin' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+        });
+    }
+};
