404f3dcbb182baabb9ffc2dfe15586ca4bcf3b1e
mpa12 <maksim.28.05@mail.ru>
2023-07-01 16:24:22 +1000

    build

---
+++ black-on-white/public/build/assets/Logout-e5b2c689.js
@@ -0,0 +1,1 @@
+import{_ as e,U as o}from"./app-bded94c7.js";const n={name:"Logout",created(){o.logout();let t=new CustomEvent("localStorageUpdated",{detail:null});window.dispatchEvent(t),this.$router.push("/")}};function r(t,s,a,c,u,d){return null}const p=e(n,[["render",r]]);export{p as default};
